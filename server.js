d971730b22bd278e8d5a84c6e8796a6bbd0f1113
Nover-WD <89575046+Nover-WD@users.noreply.github.com>
2022-11-30 21:45:15 +0800

    Nov. 30 Commit

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(express.json());
 
 /*Heroku Production*/
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join('client/build', 'index.html'));
+      });
 }
 
 // Server Home
@@ -35,14 +38,6 @@ if (process.env.NODE_ENV === 'production'){
 //     // res.send("Authorized");
 // });
 
-//Refresh Page Solution
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'),function(err) {
-      if (err) {
-        res.status(500).send(__dirname)
-      }
-    })
-});
 
 // // Fetch Items
 // app.use("/api/v1/items/", itemRoutes);
