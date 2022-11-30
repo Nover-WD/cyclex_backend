3ad046500a928ec3eda92d44a1c8da88e772df87
Nover-WD <89575046+Nover-WD@users.noreply.github.com>
2022-11-30 20:59:04 +0800

    Nov. 30 Commit

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production'){
 
 //Refresh Page Solution
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'),function(err) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'),function(err) {
       if (err) {
         res.status(500).send(__dirname)
       }
