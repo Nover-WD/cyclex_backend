db8bbde9f3b816c4cbc08013da0f21d1d2ea4693
Nover-WD <89575046+Nover-WD@users.noreply.github.com>
2023-01-02 17:52:20 +0800

    error fixes 1

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.use(express.json());
 //     app.use(express.static('client/build'));
 // }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // Server Home
 app.get("/", (req, res) => {
     res.send("Your Authorized");
